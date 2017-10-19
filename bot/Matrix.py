316eeb917e9c8a9fc6bce3736aef8962961962b3
Tom Lant <lampholder@gmail.com>
2017-10-19 15:20:40 +0100

    Slack and Matrix bits

---
+++ bot/Matrix.py
@@ -0,0 +1,78 @@
+# coding=utf-8
+"""Matrix side of the Slack migrator."""
+
+import json
+import hmac
+import hashlib
+import requests
+
+from matrix_client.client import MatrixClient
+from matrix_client.errors import MatrixRequestError
+
+class Matrix(object):
+    """For wrangling the Matrix users."""
+
+    def __init__(self, server, registration_secret):
+        self._server = server
+        self._registration_secret = registration_secret
+
+    @staticmethod
+    def passgen(mxid):
+        """Function for translating a mxid into a known-but-unguessable password.
+        Some element of this bad boy needs to be excluded from version control."""
+        return hashlib.md5(mxid + '\x00' + 'SALTY').hexdigest()
+
+    def change_password(self, mxid, old_password, new_password):
+        """Change a user's password.
+        Warning: this function is slow as balls."""
+        matrix = MatrixClient(self._server)
+        matrix.login_with_password(username=mxid,
+                                   password=old_password)
+
+        body = {
+            "auth": {
+                "type": "m.login.password",
+                "user": mxid,
+                "password": old_password
+            },
+            "new_password": new_password
+        }
+
+        try:
+            matrix.api._send('POST', '/account/password', body, api_path='/_matrix/client/r0')
+            return True
+
+        except MatrixRequestError as exception:
+            print exception
+            return False
+
+    def create_user(self, mxid, password_function=Matrix.passgen, admin=False):
+        """Creates a user - the password is generated from a function."""
+
+        mac = hmac.new(key=self._registration_secret,
+                       digestmod=hashlib.sha1)
+
+        password = password_function(mxid)
+
+        mac.update(mxid)
+        mac.update("\x00")
+        mac.update(password)
+        mac.update("\x00")
+        mac.update("admin" if admin else "notadmin")
+
+        body = {
+            "user": mxid,
+            "password": password,
+            "mac": mac.hexdigest(),
+            "type": "org.matrix.login.shared_secret",
+            "admin": admin,
+        }
+
+        response = requests.post('%s/_matrix/client/api/v1/register' % self._server,
+                                 data=json.dumps(body),
+                                 headers={'Content-Type': 'application/json'})
+
+        if response.code != 200:
+            raise Exception(response.code, response.text)
+        else:
+            return True
---
+++ bot/Slack.py
@@ -0,0 +1,57 @@
+# coding=utf-8
+"""Handles the Slack side of business."""
+
+import time
+
+from slackclient import SlackClient
+
+class Slack(object):
+    """Class for messaging the Slack users"""
+
+    def __init__(self, slack_token):
+        self._slack = SlackClient(slack_token)
+
+    def listen(self):
+        """Listens for inbound Slack events."""
+        if self._slack.rtm_connect():
+            while True:
+                output = self._slack.rtm_read()
+                for event in self._filter_slack_output(output):
+                    self._process(event)
+                time.sleep(1)
+
+    def list_users(self):
+        """Lists all the users in the attached team."""
+        return self._slack.api_call('users.list')
+
+    def direct_message(self, user, message):
+        """Send a direct message to the specified user."""
+        self._slack.api_call('chat.postMessage',
+                             channel=user,
+                             text=message,
+                             as_user=True)
+
+    @staticmethod
+    def complete_oauth(client_id, client_secret, code):
+        """Completes the oauth registrion with the code provided by slack."""
+        slack = SlackClient("")
+
+        return slack.api_call('oauth.access',
+                              client_id=client_id,
+                              client_secret=client_secret,
+                              code=code)
+
+    @staticmethod
+    def _filter_slack_output(output):
+        """We only want to react to messages, and we want to exclude certain users9
+        (such as slackbot)"""
+        barred_users = ['USLACKBOT']
+        entries_to_process = [event for event in output
+                              if event['type'] == 'message'
+                              and event['user'] not in barred_users]
+
+        return entries_to_process
+
+    def _process(self, event):
+        """Processes an inbound Slack event."""
+        self.direct_message(event['user'], 'I don\'t understand humans.')
